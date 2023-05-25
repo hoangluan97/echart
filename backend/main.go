ec101eb5c7bc79d26f4d0ae213e5391e3c15ac2b
Bioturing <bioturing@MacBook-Pro-cua-Bioturing.local>
2023-05-26 00:05:08 +0700

    add

--- backend/main.go
+++ backend/main.go
@@ -10,20 +10,22 @@ import (
 
 func initRouter() *mux.Router {
 	router := mux.NewRouter()
-
-	router.HandleFunc("/static/", chart.GetChartData)
-
+	router.HandleFunc("/static", chart.GetChartData).Methods("GET")
+	spa := chart.SpaHandler{staticPath: "build", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
 	return router
 }
 
 func main() {
 	router := initRouter()
 
+	router.Use(mux.CORSMethodMiddleware(router))
+
+	fmt.Println("connected")
 	err := http.ListenAndServe(":8080", router)
-	if err == nil {
-		fmt.Println("connected")
-	}
+
 	if err != nil {
+		fmt.Println("Error starting server:", err)
 		panic(err)
 	}
 }
--- backend/pkg/chart/data.go
+++ backend/pkg/chart/data.go
@@ -3,15 +3,58 @@ package chart
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
+
+type SpaHandler struct {
+	staticPath string
+	indexPath  string
+}
 
 func GetChartData(response http.ResponseWriter, request *http.Request) {
 	chartType := request.URL.Query().Get("type")
 	dataFile := chartType + ".json"
-	fmt.Println("dataFile:", dataFile)
-	fmt.Println("rootDir:", "/static/"+dataFile+".json")
+	fmt.Println("rootDir:", "/static/"+dataFile)
+	allowOrigin(response)
+	http.ServeFile(response, request, "static/"+dataFile)
+}
+
+func (h SpaHandler) RenderHomePage(response http.ResponseWriter, request *http.Request) {
+	path, err := filepath.Abs(request.URL.Path)
+	fmt.Println("path:", path)
+
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+	fmt.Println("path pretend:", path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(response, request, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(response, request)
+}
+
+func allowOrigin(response http.ResponseWriter) {
 	response.Header().Set("Content-Type", "application/json")
 	response.Header().Set("Access-Control-Allow-Origin", "*")
 	response.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	http.ServeFile(response, request, "static/"+dataFile)
+	response.Header().Set("Access-Control-Allow-Methods", http.MethodGet)
 }
