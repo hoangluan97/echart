c8f66e15a84705edec84c2ff7c8be634518274c9
Bioturing <bioturing@MacBook-Pro-cua-Bioturing.local>
2023-05-25 14:20:08 +0700

    add

--- backend/main.go
+++ backend/main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/hoangluan97/echart/backend/pkg/chart"
 )
 
-
 func initRouter() *mux.Router {
 	router := mux.NewRouter()
 
-    router.HandleFunc("/static/", chart.GetChartData)
+	router.HandleFunc("/static/", chart.GetChartData)
 
 	return router
 }
 
 func main() {
-	router : = initRouter()
+	router := initRouter()
 
 	err := http.ListenAndServe(":8080", router)
 
